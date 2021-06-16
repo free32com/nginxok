7ee93b8c55e71d59508a8332ecbca8f643729d79
root <magicsoma@hanmail.net>
2021-06-16 16:27:04 +0900

    initial push

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-### tsharp Docker
-
-#docker run -it --privileged -d --name=nginx -p 9000:80 -v /volume2/Data/tsharp/userdata:/var/www/html --restart unless-stopped nginx:latest
-docker run -dit --privileged -d --name=nginx -p 80:80 --restart unless-stopped nginx:test 
-
